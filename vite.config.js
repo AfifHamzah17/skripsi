07212c76b4beb59bd4d3c41af3f5a339f73fef0a
unknown <afifhamzah10@gmail.com>
2026-04-02 22:05:00 +0700

    api

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://skripsi-api-995782183824.asia-southeast2.run.app',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE.replace('/api', ''),
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
