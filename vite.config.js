b786c527fef5f80ff659b8783d0f305514b79997
unknown <afifhamzah10@gmail.com>
2026-04-02 22:12:14 +0700

    proxy 2

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://skripsi-api-995782183824.asia-southeast2.run.app',
+        target: 'https://skripsi-api-995782183824.asia-southeast2.run.app/api/',
         changeOrigin: true,
         secure: false,
       },
